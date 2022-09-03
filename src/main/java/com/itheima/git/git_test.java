65bcf7520de4177584b734def3a84c45008d9736
yang19981028 <112691933+yang19981028@users.noreply.github.com>
2022-09-03 13:04:45 +0800

    pull commit

--- src/main/java/com/itheima/git/git_test.java
+++ src/main/java/com/itheima/git/git_test.java
@@ -9,5 +9,6 @@ public class git_test {
         System.out.println("master test");
         System.out.println("hotfix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
