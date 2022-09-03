299c2c665e7360e6b0fe25c8986386ecccb84aac
yang <1075164909@qq.com>
2022-09-03 11:58:56 +0800

    second commit

--- src/main/java/com/itheima/git/git_test.java
+++ src/main/java/com/itheima/git/git_test.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
