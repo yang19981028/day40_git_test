31c6574cb02a361b4beacb51d12d7868da1692ae
yang <1075164909@qq.com>
2022-09-03 12:35:52 +0800

    hot-fix commit2

--- src/main/java/com/itheima/git/git_test.java
+++ src/main/java/com/itheima/git/git_test.java
@@ -6,5 +6,6 @@ public class git_test {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hotfix test");
     }
 }
